e0a9186e0f5a590876583d7943a93908b1ab01aa
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2018-11-17 20:45:08 -0500

    Proxy,Facade,Adapter

---
+++ endpoints-v2-backend/src/main/java/com/example/echo/Proxy.java
@@ -0,0 +1,63 @@
+package com.example.echo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Proxy implements IProxy {
+
+	private Facade facade = Facade.rConstructora();
+	private static Proxy instanciaUnica = null;
+
+	private ArrayList<IUsuario> usuarios = new ArrayList();
+
+	public static Proxy rConstructora() {
+		if (instanciaUnica == null) {
+			instanciaUnica = new Proxy();
+		}
+		return instanciaUnica;
+	}
+
+	@Override
+	public Session auth(String user, String pass) {
+		Random ran = new Random();
+		for (IUsuario us : usuarios) {
+			if (us.getCorreo().equals(user)&&us.getPass().equals(pass)) {
+				Session session = new Session();
+				session.setSession(ran.nextLong());
+				facade.guardarSesion(session);
+				return session;
+			}
+
+		}
+
+		return null;
+	}
+
+	@Override
+	public Session test(long sesion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void crearPasajero(String correo, String password, String nombre, String apellido, int edad) {
+		IUsuario user=new Pasajero();
+		user.setCorreo(correo);
+		user.setPass(password);
+		user.setNombre(nombre);
+		user.setApellido(apellido);
+		user.setEdad(edad);
+		usuarios.add(user);
+	}
+
+	@Override
+	public void crearConductor(String correo, String password, String nombre, String apellido, int edad) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+
+}
