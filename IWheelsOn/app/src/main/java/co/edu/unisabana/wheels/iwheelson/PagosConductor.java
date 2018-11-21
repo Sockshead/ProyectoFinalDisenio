05dbb154cdde1d5571d63f946c510e95cff67eff
Tato2101 <38194451+Tato2101@users.noreply.github.com>
2018-11-20 23:13:28 -0500

    mas pantallas

---
+++ IWheelsOn/app/src/main/java/co/edu/unisabana/wheels/iwheelson/PagosConductor.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.wheels.iwheelson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagosConductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagos_conductor);
+    }
+}
